faeb59f7a2eed8fe3a33d2f18232afce10d63895
Nafiya Shaik <nafiya7867@hotmail.com>
2019-09-05 12:17:22 -0500

    hw1

---
+++ Eda_1820715_HW1/Eda_1820715_HW1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eda_1820715_HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter first number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("enter second number");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the operator:(+,-,/,*)");
+            string op = Console.ReadLine();
+            if (op == "+")
+            {
+                int sum;
+                sum = num1 + num2;
+                Console.WriteLine($"sum is {sum}");
+            }
+            if (op == "-")
+            {
+                int subtract;
+                subtract = num1 - num2;
+                Console.WriteLine($"subtraction is {subtract}");
+            }
+            if (op == "*")
+            {
+                int multiplication;
+                multiplication = num1 * num2;
+                Console.WriteLine($"multiplication is {multiplication}");
+            }
+            if (op == "/")
+            {
+                int division;
+                division = num1 / num2;
+                Console.WriteLine($"division is {division}");
+            }
+        }
+    }
+}
